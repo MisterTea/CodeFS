b0286a83f38a58040cba8bf57dc4c554f73ce38f
Jason Gauci <jjg@fb.com>
2019-04-28 16:45:37 -0700

    Set ZMQ Rpc as reliable

--- src/base/ZmqBiDirectionalRpc.cpp
+++ src/base/ZmqBiDirectionalRpc.cpp
@@ -2,7 +2,7 @@
 
 namespace codefs {
 ZmqBiDirectionalRpc::ZmqBiDirectionalRpc(const string& _address, bool _bind)
-    : BiDirectionalRpc(false), address(_address), bind(_bind) {
+    : BiDirectionalRpc(true), address(_address), bind(_bind) {
   context = shared_ptr<zmq::context_t>(new zmq::context_t(4));
   socket =
       shared_ptr<zmq::socket_t>(new zmq::socket_t(*(context.get()), ZMQ_PAIR));
