0cd72b2e73ae7a9c51649aff9c5965f6edf29d48
Jason Gauci <jjg@fb.com>
2019-05-08 15:41:14 -0700

    build on linux

--- external/fswatch_1.9.3/libfswatch_config.h
+++ external/fswatch_1.9.3/libfswatch_config.h
@@ -136,9 +136,6 @@
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "1.14.0"
 
-/* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS (1)
-
 /* Version number of package */
 #define VERSION "1.14.0"
 
